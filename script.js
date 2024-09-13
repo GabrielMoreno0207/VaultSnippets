d786af8f9bef77b1c1e9c98f71e298c2590751ce
Gabriel Moreno <gabrielmfrontend@hotmail.com>
2024-09-13 11:23:48 -0300

    Revert "v3"
    This reverts commit 55b4e34cf4fb84682903a70a187637e5f99f6b32.

--- script.js
+++ script.js
@@ -1,141 +1,125 @@
 document.addEventListener('DOMContentLoaded', function () {
-  var editor = CodeMirror.fromTextArea(document.getElementById('code'), {
-      lineNumbers: true,
-      mode: 'javascript', // Mude para o modo desejado
-      theme: 'dracula' // Mude para o tema desejado
-  });
-
   let clipboard = new ClipboardJS('.copy-btn');
 
   clipboard.on('success', function (e) {
-      $('#copyModal').modal('show');
-      e.clearSelection();
+    $('#copyModal').modal('show');
+    e.clearSelection();
   });
 
   clipboard.on('error', function (e) {
-      console.error('Falha ao copiar o texto: ', e);
+    console.error('Falha ao copiar o texto: ', e);
   });
-
-  // Função para escapar caracteres especiais
-  function escapeHtml(unsafe) {
-      return unsafe
-          .replace(/&/g, "&amp;")
-          .replace(/</g, "&lt;")
-          .replace(/>/g, "&gt;")
-          .replace(/"/g, "&quot;")
-          .replace(/'/g, "&#039;");
-  }
 
   // Adicionando novo snippet
   document.getElementById('snippetForm').addEventListener('submit', function (e) {
-      e.preventDefault();
+    e.preventDefault();
 
-      const title = document.getElementById('title').value;
-      const code = editor.getValue(); // Obtendo o código do CodeMirror
-      const language = document.getElementById('language').value;
+    const title = document.getElementById('title').value;
+    const code = document.getElementById('code').value;
+    const language = document.getElementById('language').value;
 
-      fetch('http://localhost:3000/snippets', {
-          method: 'POST',
-          headers: {
-              'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({ title, code, language })
-      })
+    fetch('http://localhost:3000/snippets', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ title, code, language })
+    })
       .then(response => response.json())
       .then(data => {
-          const snippetList = document.querySelector('.snippet-list');
-          const snippetItem = document.createElement('div');
-          snippetItem.className = 'card snippet-item';
+        const snippetList = document.querySelector('.snippet-list');
+        const snippetItem = document.createElement('div');
+        snippetItem.className = 'card snippet-item';
 
-          snippetItem.innerHTML = `
-              <div class="card-body">
-                  <h5 class="card-title">${data.title}</h5>
-                  <p><strong>Linguagem:</strong> ${data.language}</p>
-                  <pre><code>${escapeHtml(data.code)}</code></pre>
-                  <button class="btn btn-secondary copy-btn" data-clipboard-text="${encodeURIComponent(data.code)}">Copiar</button>
-              </div>
-          `;
+        snippetItem.innerHTML = `
+          <div class="card-body">
+            <h5 class="card-title">${data.title}</h5>
+            <p><strong>Linguagem:</strong> ${data.language}</p>
+            <pre><code>${data.code}</code></pre>
+            <button class="btn btn-secondary copy-btn" data-clipboard-text="${encodeURIComponent(data.code)}">Copiar</button>
+          </div>
+        `;
 
-          snippetList.appendChild(snippetItem);
+        snippetList.appendChild(snippetItem);
 
-          $('#addModal').modal('show');
-          document.getElementById('snippetForm').reset();
+        $('#addModal').modal('show');
+        document.getElementById('snippetForm').reset();
 
-          // Atualiza a funcionalidade do botão de copiar
-          clipboard.destroy();
-          clipboard = new ClipboardJS('.copy-btn');
+        // Atualiza a funcionalidade do botão de copiar
+        clipboard.destroy();
+        clipboard = new ClipboardJS('.copy-btn');
       });
   });
 
   // Carregando snippets existentes e exibindo-os
   fetch('http://localhost:3000/snippets')
-      .then(response => response.json())
-      .then(snippets => {
-          const snippetList = document.querySelector('.snippet-list');
-          snippets.forEach(snippet => {
-              const snippetItem = document.createElement('div');
-              snippetItem.className = 'card snippet-item';
+    .then(response => response.json())
+    .then(snippets => {
+      const snippetList = document.querySelector('.snippet-list');
+      snippets.forEach(snippet => {
+        const snippetItem = document.createElement('div');
+        snippetItem.className = 'card snippet-item';
 
-              snippetItem.innerHTML = `
-                  <div class="card-body">
-                      <h5 class="card-title">${snippet.title}</h5>
-                      <p><strong>Linguagem:</strong> ${snippet.language}</p>
-                      <pre><code>${escapeHtml(snippet.code)}</code></pre>
-                      <button class="btn btn-secondary copy-btn" data-clipboard-text="${encodeURIComponent(snippet.code)}">Copiar</button>
-                  </div>
-              `;
+        snippetItem.innerHTML = `
+          <div class="card-body">
+            <h5 class="card-title">${snippet.title}</h5>
+            <p><strong>Linguagem:</strong> ${snippet.language}</p>
+            <pre><code>${snippet.code}</code></pre>
+            <button class="btn btn-secondary copy-btn" data-clipboard-text="${encodeURIComponent(snippet.code)}">Copiar</button>
+          </div>
+        `;
 
-              snippetList.appendChild(snippetItem);
-          });
-
-          // Atualiza a funcionalidade do botão de copiar
-          clipboard.destroy();
-          clipboard = new ClipboardJS('.copy-btn');
+        snippetList.appendChild(snippetItem);
       });
+
+      // Atualiza a funcionalidade do botão de copiar
+      clipboard.destroy();
+      clipboard = new ClipboardJS('.copy-btn');
+    });
 
   // Filtro de busca e linguagem
   const searchInput = document.getElementById('search');
   const searchLanguage = document.getElementById('searchLanguage');
 
   function filterSnippets() {
-      const searchTerm = searchInput.value.toLowerCase();
-      const selectedLanguage = searchLanguage.value;
+    const searchTerm = searchInput.value.toLowerCase();
+    const selectedLanguage = searchLanguage.value;
 
-      // Limpa a lista de snippets antes de aplicar o filtro
-      const snippetList = document.querySelector('.snippet-list');
-      snippetList.innerHTML = '';
+    // Limpa a lista de snippets antes de aplicar o filtro
+    const snippetList = document.querySelector('.snippet-list');
+    snippetList.innerHTML = '';
 
-      fetch('http://localhost:3000/snippets')
-          .then(response => response.json())
-          .then(snippets => {
-              const filteredSnippets = snippets.filter(snippet => {
-                  const matchesTitle = snippet.title.toLowerCase().includes(searchTerm);
-                  const matchesLanguage = selectedLanguage === '' || snippet.language === selectedLanguage;
+    fetch('http://localhost:3000/snippets')
+      .then(response => response.json())
+      .then(snippets => {
+        const filteredSnippets = snippets.filter(snippet => {
+          const matchesTitle = snippet.title.toLowerCase().includes(searchTerm);
+          const matchesLanguage = selectedLanguage === '' || snippet.language === selectedLanguage;
 
-                  return matchesTitle && matchesLanguage;
-              });
+          return matchesTitle && matchesLanguage;
+        });
 
-              // Exibe os snippets filtrados
-              filteredSnippets.forEach(snippet => {
-                  const snippetItem = document.createElement('div');
-                  snippetItem.className = 'card snippet-item';
+        // Exibe os snippets filtrados
+        filteredSnippets.forEach(snippet => {
+          const snippetItem = document.createElement('div');
+          snippetItem.className = 'card snippet-item';
 
-                  snippetItem.innerHTML = `
-                      <div class="card-body">
-                          <h5 class="card-title">${snippet.title}</h5>
-                          <p><strong>Linguagem:</strong> ${snippet.language}</p>
-                          <pre><code>${escapeHtml(snippet.code)}</code></pre>
-                          <button class="btn btn-secondary copy-btn" data-clipboard-text="${encodeURIComponent(snippet.code)}">Copiar</button>
-                      </div>
-                  `;
+          snippetItem.innerHTML = `
+            <div class="card-body">
+              <h5 class="card-title">${snippet.title}</h5>
+              <p><strong>Linguagem:</strong> ${snippet.language}</p>
+              <pre><code>${snippet.code}</code></pre>
+              <button class="btn btn-secondary copy-btn" data-clipboard-text="${encodeURIComponent(snippet.code)}">Copiar</button>
+            </div>
+          `;
 
-                  snippetList.appendChild(snippetItem);
-              });
+          snippetList.appendChild(snippetItem);
+        });
 
-              // Atualiza a funcionalidade do botão de copiar
-              clipboard.destroy();
-              clipboard = new ClipboardJS('.copy-btn');
-          });
+        // Atualiza a funcionalidade do botão de copiar
+        clipboard.destroy();
+        clipboard = new ClipboardJS('.copy-btn');
+      });
   }
 
   // Eventos de input para busca e mudança de seleção da linguagem
